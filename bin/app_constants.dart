ee259db874ebed1f6124d45772ec14f4dca26d36
getBoolean <danielwnewville@gmail.com>
2021-08-28 14:30:36 -0500

    Initial commit

---
+++ bin/app_constants.dart
@@ -0,0 +1,18 @@
+class AppConstant {
+
+  // Appwrite
+  // Mac
+  // static const String url = 'http://129.107.97.163/';
+  // static const String endpoint = 'http://129.107.97.163/v1';
+  // static const String project = '60bbe0b4363f5';
+  // Desktop
+  static const String url = 'http://172.24.240.1';
+  static const String endpoint = 'http://172.24.240.1/v1';
+  static const String project = '611ffbae6bdd9';
+
+  // Appwrite Database
+  static const String readingStatsCollection = '61205e62f3bc5';
+  static const String booksCollection = '61205e630fbcb';
+  static const String shelvesCollection = '61205e636767b';
+
+}
