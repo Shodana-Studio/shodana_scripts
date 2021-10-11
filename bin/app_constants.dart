074236abe276911c2c7267b315f42b01459279d8
getBoolean <danielwnewville@gmail.com>
2021-10-11 17:35:45 -0500

    Update with new url

--- bin/app_constants.dart
+++ bin/app_constants.dart
@@ -6,9 +6,9 @@ class AppConstant {
   // static const String endpoint = 'http://129.107.97.163/v1';
   // static const String project = '60bbe0b4363f5';
   // Desktop
-  static const String url = 'http://localhost';
-  static const String endpoint = 'http://localhost/v1';
-  static const String project = '611ffbae6bdd9';
+  static const String url = 'http://192.168.1.203';
+  static const String endpoint = 'http://192.168.1.203/v1';
+  static const String project = '615a678115f4d';
 
   // Appwrite Database
   static const String readingStatsCollection = '613435387a69b';
